03a5b5b60eac3092a2504eebc87238a04675b9a8
Евгений <nk-nazarovEA@yandex.ru>
2026-01-11 00:15:12 +0300

    добавил два теста для CatTest.java подредактировал pom

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,23 +1,28 @@
 import com.example.Feline;
 import com.example.Cat;
+import com.example.Predator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import java.util.List;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testng.annotations.Test;
 
-@RunWith(MockitoJUnitRunner.class)
-
-
+@ExtendWith(MockitoExtension.class)
 public class CatTest {
     @Mock
-    Feline feline;
+     Feline feline;
 
     @Test
-    feline.getSound("Мяу"); // вызвали метод объекта с аргументом
-    Mockito.verify(feline).getSound("Мяу");
-
-
+    public void getSoundTest() { // сам тест
+        cat.getSound("Мяу"); // вызвали метод объекта с аргументом
+        Mockito.verify(feline).getSound("Мяу");
+    }
+    @Test
+    public void getFoodTest() throws Exception {
+        cat.getFood(predator.eatMeat()); // вызвали метод объекта с аргументом
+        Mockito.verify(feline).getSound(predator.eatMeat());
+    }
 }
