bcbcde69cbd79e9b15578321c5aff1010915fd3c
Евгений <nk-nazarovEA@yandex.ru>
2026-01-11 00:45:28 +0300

    добавил два теста на колличество котят

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,21 +1,31 @@
+import com.example.Feline;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
 @Test
     public void eatMeatTest() throws Exception {
+    Feline feline = new Feline();
 
     }
 @Test
     public void getFamilyTest() {
-
+    Feline feline = new Feline();
     }
 @Test
     public void getKittensTest() {
-
+    Feline feline = new Feline();
+    int expectedKittensCount = 1;
+    int actualKittensCount = feline.getKittens();
+    assertEquals("не верное кол-во котят", expectedKittensCount, actualKittensCount);
     }
 @Test
-    public void getKittensTest() {
-
+    public void getCountManyKittensTest() {
+    Feline feline = new Feline();
+    int expectedKittensCount = 1;
+    int actualKittensCount = feline.getKittens(2);
+    assertEquals("не верное кол-во котят", expectedKittensCount, actualKittensCount);
     }
 
 }
