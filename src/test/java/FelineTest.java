96a17c3e405934fb9e599651f75d96a48f76b665
Евгений <nk-nazarovEA@yandex.ru>
2026-01-11 01:10:23 +0300

    добавил тест на определение семейства

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,13 +1,25 @@
 import com.example.Feline;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
 @Test
-    public void eatMeatTest() throws Exception {
+    public void eatRightMeatTest() throws Exception {
     Feline feline = new Feline();
-
+    List<String> expectedFood = Collections.singletonList("Рыба");
+    List<String> actualFood = feline.eatMeat();
+    assertEquals("Кошачьи едят правильную пищу", expectedFood, actualFood);
+    }
+    @Test
+    public void eatNotRightMeatTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualFood = feline.eatMeat();
+        assertEquals("Кошачьи едят не правильную пищу", expectedFood, actualFood);
     }
 @Test
     public void getFamilyTest() {
@@ -26,9 +38,7 @@ public class FelineTest {
 @Test
     public void getCountManyKittensTest() {
     Feline feline = new Feline();
-    int expectedKittensCount = 1;
-    int actualKittensCount = feline.getKittens(2);
-    assertEquals("не верное кол-во котят", expectedKittensCount, actualKittensCount);
+    assertEquals(3, feline.getKittens(3));
     }
 
     @Test
