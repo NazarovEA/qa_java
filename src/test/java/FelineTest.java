4429ea2a45fb037aabdf10c5a2d9d477c8e206b3
Евгений <nk-nazarovEA@yandex.ru>
2026-01-11 00:33:27 +0300

    заготовка тестов FelineTest

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class FelineTest {
+@Test
+    public void eatMeatTest() throws Exception {
+
+    }
+@Test
+    public void getFamilyTest() {
+
+    }
+@Test
+    public void getKittensTest() {
+
+    }
+@Test
+    public void getKittensTest() {
+
+    }
+
+}
