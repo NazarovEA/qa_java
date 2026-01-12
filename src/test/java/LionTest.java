5a344a23a010d120985eef075b1e0a733d1ea3cc
Евгений <nk-nazarovEA@yandex.ru>
2026-01-13 00:19:37 +0300

    добавил Моск в лион

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,21 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+@Mock
+    Feline feline;
+    public void getKittensTest(){
+
+    }
+    public void doesHaveManeTest(){
+
+    }
+    public void getFoodTest() throws Exception {
+
+    }
 }
