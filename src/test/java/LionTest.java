75371362c5745e639b085152e640cbdc432d2782
Евгений <nk-nazarovEA@yandex.ru>
2026-01-13 23:00:57 +0300

    дописал тесты Lion

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,21 +1,40 @@
 import com.example.Feline;
 import com.example.Lion;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 @Mock
     Feline feline;
-    public void getKittensTest(){
-
+@Test
+    public void getKittensTest() throws Exception {
+Lion lion = new Lion("Самка", feline);
+    Mockito.when(feline.getKittens()).thenReturn(2);
+    int expectedKittensCount = 2;
+    int actualKittensCount = lion.getKittens();
+    assertEquals(expectedKittensCount, actualKittensCount);
     }
-    public void doesHaveManeTest(){
-
+    @Test
+    public void doesHaveManeTest() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        boolean expectedHaveMane = true;
+        boolean actualHaveMane = lion.doesHaveMane();
+        assertEquals(expectedHaveMane, actualHaveMane);
     }
+    @Test
     public void getFoodTest() throws Exception {
-
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getFood("Трава")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        List<String> actual = lion.getFood();
+        assertEquals(expected, actual);
     }
 }
